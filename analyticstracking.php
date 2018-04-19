6a112ab19dfce8d5eee5070ecd2d83f8cf4e574a
DeValladolid <devalladolidoficial@gmail.com>
2018-04-19 23:41:18 +0200

    Update analyticstracking.php

--- analyticstracking.php
+++ analyticstracking.php
@@ -7,3 +7,11 @@
 
   gtag('config', 'UA-71246365-14');
 </script>
+<script>
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
+
+  _gs('GSN-889729-U');
+</script>
