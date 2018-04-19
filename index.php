0859b9479ff49c59a133a643639f30c1d757a9b9
DeValladolid <devalladolidoficial@gmail.com>
2018-04-19 23:16:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -158,6 +158,14 @@ if (preg_match('/^(a|gallery)\/([a-zA-Z0-9]{5,})$/i', $imgur, $matches)) {
   </head>
   <body>
 <?php foreach($images as $image) { ?>
+    <script>
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
+
+  _gs('GSN-889729-U');
+</script>
     <a href="/<?=$image->hash.$image->ext?>">
       <img src="/<?=$image->hash.'h'.$image->ext?>" >
       <span><?=$image->description?></span>
